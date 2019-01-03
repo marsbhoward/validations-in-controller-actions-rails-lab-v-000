4a1684f52c53d01de570972f3712ddded58ad108
Mars Howard <marsbhoward@gmail.com>
2019-01-03 18:50:31 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,16 +8,14 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
-  
-      if !@author.valid?
-        render :new
-      else
-        @author.save
-        redirect_to author_path(@author)
-      else
-        render :new
-      end
+
+    if !@author.valid?
+      render :new
+    else
+      @author.save
+      redirect_to author_path(@author)
     end
+  end
 
   private
 
